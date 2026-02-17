b10d07a32ec7134bac5862280e2fbdd52e4defef
木村輝 <hikarudjent@gmail.com>
2026-02-17 23:03:28 +0900

    修正

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
+import { baseUrl } from "./base-url";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: baseUrl(),
 });
